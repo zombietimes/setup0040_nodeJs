c22f6bb973c56a5b54cd43cafb0fb3f150e64309
royal <royal@yakinkuclub.com>
2019-05-04 21:13:04 +0900

    2019/05/04

---
+++ ubuntuCmd_setupNodeJs.sh
@@ -0,0 +1,16 @@
+# This script is for Ubuntu Ver18.04.
+#!/bin/sh
+
+echo "\n<Setup>"
+echo "Install nodejs"
+echo "\n"
+sudo apt-get install nodejs -y
+node --version
+
+echo "\n<Setup>"
+echo "Install npm"
+echo "\n"
+sudo apt install npm -y
+npm --version
+
+
